98044803e49c4b98ce8b572fd966509f908f0e33
root <pokestory.root@gmail.com>
2026-03-22 00:27:04 +0900

    debug

--- esphome/components/panaac/panaac.cpp
+++ esphome/components/panaac/panaac.cpp
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ #define USE_CLIMATE_VISUAL_OVERRIDES 1
 
  #include "panaac.h"
- #define USE_CLIMATE_VISUAL_OVERRIDES 1
  #include "esphome/components/climate/climate.h"
 
 namespace esphome
