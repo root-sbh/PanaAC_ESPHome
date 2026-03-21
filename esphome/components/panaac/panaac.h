be3460a491e0c9f778f84d49fa6977eb9d4706c8
root <pokestory.root@gmail.com>
2026-03-21 23:45:34 +0900

    debug

--- esphome/components/panaac/panaac.h
+++ esphome/components/panaac/panaac.h
@@ -91,7 +91,7 @@ namespace esphome
 
             ESPPreferenceObject status_pref_;
             //7で良いけど今後の拡張時のために10にしておく
-            std::array<ClimateState, 10> *status_list_ = { ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState(),ClimateState() };
+            std::array<ClimateState, 10> *status_list_;
 
             void save_pref();
         };
